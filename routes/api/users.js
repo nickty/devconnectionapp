1ddf0eee8fbb115d91109febf29a29ff46c0c49f
Mizanur Rahman <nickty.86@gmail.com>
2020-12-20 10:25:33 +0600

    token is back from user

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router(); 
 const bcrypt = require('bcryptjs')
 const gravatar = require('gravatar')
+const jwt =require('jsonwebtoken')
+const config = require('config')
 const {check, validationResult} = require('express-validator/check')
 
 const User = require('../../models/User')
@@ -32,7 +34,7 @@ router.post('/', [
 
         let user = await User.findOne({email})
         if(user){
-            res.status(400).json({errors: [{msg: 'User Already Exists'}]})
+            return res.status(400).json({errors: [{msg: 'User Already Exists'}]})
         }
 
         //get user gravater
@@ -52,6 +54,9 @@ router.post('/', [
             password
         });
 
+       
+        //entrypt password 
+
         const salt = await bcrypt.genSalt(10); 
         user.password = await bcrypt.hash(password, salt)
        
@@ -59,12 +64,24 @@ router.post('/', [
 
         await user.save()
 
-        //entrypt password 
 
         //return jsonwebtoken
 
-        res.send("User Registered")
-        
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 360000}, (err, token) => {
+            if(err){
+                throw err; 
+            } else {
+                res.json({token})
+            }
+        })
+
+               
     } catch (err) {
         console.error(err.message)
         res.status(500).send('Server Error')
